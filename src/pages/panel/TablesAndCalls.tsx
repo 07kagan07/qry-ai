43efed76538c1c5edb4c60453f368b4f41aaa455
07kagan07 <kagancoskun32@gmail.com>
2026-08-05 20:25:19 +0300

    volume up

--- src/pages/panel/TablesAndCalls.tsx
+++ src/pages/panel/TablesAndCalls.tsx
@@ -36,7 +36,7 @@ function beep() {
     osc.connect(gain)
     gain.connect(ctx.destination)
     osc.frequency.value = 880
-    gain.gain.setValueAtTime(0.15, ctx.currentTime)
+    gain.gain.setValueAtTime(0.6, ctx.currentTime)
     gain.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + 0.4)
     osc.start()
     osc.stop(ctx.currentTime + 0.4)
